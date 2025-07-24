42a599220d15cd17252650dd453bd35194d8b6af
Darpan Pandey <p.darpan@tbc.edu.np>
2025-07-24 17:59:40 +0545

    Added a Consumer class for Kafka that recieves the messages sent through the producer as well as Added necessary cofigurations for the Kafka Key and Value Serialization and De-serialization in application.yml

---
+++ src/test/java/com/jpmc/midascore/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
+    public void listen(Transaction transaction) {
+        logger.info("Received message: {}", transaction);
+    }
+}
